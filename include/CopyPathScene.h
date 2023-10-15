7860f482eba66a36ad86c0bb231f27828385ff8e
BobFredGard <bobfred@gmx.fr>
2023-10-15 13:09:17 +0200

    Version 1.0.1

--- include/CopyPathScene.h
+++ include/CopyPathScene.h
@@ -3,11 +3,11 @@ void CopyPathScene () {
     if (calculPathScene  [0] [encoder1_copy_source] == encoder1_copy_source) {
         Serial.println(encoder1_copy_source);
         tx2 = calculPathScene [2] [calculPathScene [0] [encoder1_copy_source]];
-        tx3 = calculPathScene [1] [calculPathScene [0] [encoder1_copy_source]];
+        tx3 = calculPathScene [1] [calculPathScene [0] [encoder1_copy_source]] - 1;
     }
     else {
         Serial.println(encoder1_copy_source);
-        tx3 = calculPathScene [1] [calculPathScene [0] [encoder1_copy_source]];
+        tx3 = calculPathScene [1] [calculPathScene [0] [encoder1_copy_source]] - 1;
     }
     LCD.setCursor(13,0);
     text2 = String(tx2) + "-" + String(tx3);
