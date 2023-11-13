b00485f30f0185813d0389e00930692cf487ebe7
BobFredGard <bobfred@gmx.fr>
2023-11-13 08:00:08 +0100

    rien enter the commit message for your changes. Lines starting

--- data/PrgChang.sql
+++ data/PrgChang.sql
@@ -2,157 +2,250 @@ INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
-
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
+INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
 INSERT INTO "base"."stomps" ("stomps_id") VALUES (NULL);
